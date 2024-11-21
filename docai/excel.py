20840d3c5f38a4bf82b201dd0abafe5795c73daf
yeungadrian <yeungadrianjy@gmail.com>
2024-11-21 16:07:54 +0000

    rename vars

--- docai/excel.py
+++ docai/excel.py
@@ -10,11 +10,11 @@ def parse_excel(file: IO[bytes], filename: str) -> list[Document]:
     workbook = CalamineWorkbook.from_object(file)
     documents = []
     for sheet_name in workbook.sheet_names:
-        sheet = workbook.get_sheet_by_name(sheet_name)
-        table = sheet.to_python(skip_empty_area=False)
+        calamine_sheet = workbook.get_sheet_by_name(sheet_name)
+        tabular_data = calamine_sheet.to_python(skip_empty_area=False)
         documents.append(
             Document(
-                content=tabulate(table, tablefmt="github", showindex="always"),
+                content=tabulate(tabular_data, tablefmt="github", showindex="always"),
                 metadata=MetaData(sheet_name=sheet_name, filename=filename),
             )
         )
