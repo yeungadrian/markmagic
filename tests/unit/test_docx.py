30cf64c86ec36563d3dad4b7fc961c5697907734
yeungadrian <yeungadrianjy@gmail.com>
2024-11-23 22:53:43 +0000

    fix test function naming

--- tests/unit/test_docx.py
+++ tests/unit/test_docx.py
@@ -3,7 +3,7 @@ from io import BytesIO
 from docai.docx import convert_docx
 
 
-def test_convert_excel(msft_pr_docx: tuple[BytesIO, str]) -> None:
+def test_convert_docx(msft_pr_docx: tuple[BytesIO, str]) -> None:
     """
     Test parse docx function.
 
