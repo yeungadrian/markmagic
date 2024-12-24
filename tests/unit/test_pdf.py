6b56384704a6426e9bb512654a51ebd165ddc80f
yeungadrian <yeungadrianjy@gmail.com>
2024-12-24 17:18:44 +0000

    add test for pdf

---
+++ tests/unit/test_pdf.py
@@ -0,0 +1,13 @@
+from io import BytesIO
+from pathlib import Path
+
+from docai.pdf import convert_pdf
+
+
+def test_convert_pdf() -> None:
+    """Test convert pdf function."""
+    filename = "tests/data/pdf/msft_ar.pdf"
+    markdown = convert_pdf(BytesIO(Path(filename).read_bytes()))
+    with Path("tests/data/pdf/msft_ar.md").open() as f:
+        expected_results = f.read()
+    assert markdown == expected_results
