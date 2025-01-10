98b3e2e345ea5efb3ad300be1e206c17c7fc54b6
yeungadrian <yeungadrianjy@gmail.com>
2025-01-10 18:02:24 +0000

    feature:add attachments for eml

--- docai/auto.py
+++ docai/auto.py
@@ -4,19 +4,29 @@ from io import BytesIO
 
 import puremagic
 
-from docai import convert_docx, convert_excel, convert_pdf
+from docai import convert_docx, convert_eml, convert_excel, convert_pdf
 
 
 def convert_auto(filename: str, content: bytes) -> tuple[str, str]:
     """Convert any file."""
     ext = puremagic.ext_from_filename(filename)  # type: ignore
     _content = BytesIO(content)
+    markdown = ""
     match ext:
         case ".xlsx":
-            return ".xlsx", convert_excel(_content)
+            markdown = convert_excel(_content)
         case ".docx":
-            return ".docx", convert_docx(_content)
+            markdown = convert_docx(_content)
         case ".pdf":
-            return ".pdf", convert_pdf(_content)
+            markdown = convert_pdf(_content)
+        case ".eml":
+            _markdown, attachments = convert_eml(_content)
+            markdown += _markdown
+            for attachment in attachments:
+                markdown += f"\n\n## Filename: {attachment.filename}\n\n"
+                _, _markdown = convert_auto(attachment.filename, attachment.content)
+                markdown += _markdown
         case _:
-            return "unknown", ""
+            ext = "unknown"
+            markdown = ""
+    return ext, markdown
