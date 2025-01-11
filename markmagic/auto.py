8f2f49da72cd40ea8b5799cded95d1d615a7fe2a
yeungadrian <yeungadrianjy@gmail.com>
2025-01-11 23:21:31 +0000

    feature: improve settings

--- markmagic/auto.py
+++ markmagic/auto.py
@@ -5,26 +5,29 @@ from io import BytesIO
 import puremagic
 
 from markmagic import convert_docx, convert_eml, convert_excel, convert_pdf
+from markmagic.settings import Settings
 
 
-def convert_auto(filename: str, content: bytes) -> tuple[str, str]:
+def convert_auto(filename: str, content: bytes, settings: Settings | None = None) -> tuple[str, str]:
     """Convert any file."""
+    if settings is None:
+        settings = Settings()
     ext = puremagic.ext_from_filename(filename)
     _content = BytesIO(content)
     markdown = ""
     match ext:
         case ".xlsx":
-            markdown = convert_excel(_content)
+            markdown = convert_excel(_content, settings)
         case ".docx":
-            markdown = convert_docx(_content)
+            markdown = convert_docx(_content, settings)
         case ".pdf":
-            markdown = convert_pdf(_content)
+            markdown = convert_pdf(_content, settings)
         case ".eml":
-            _markdown, attachments = convert_eml(_content)
+            _markdown, attachments = convert_eml(_content, settings)
             markdown += _markdown
             for attachment in attachments:
                 markdown += f"\n\n## Filename: {attachment.filename}\n\n"
-                _, _markdown = convert_auto(attachment.filename, attachment.content)
+                _, _markdown = convert_auto(attachment.filename, attachment.content, settings)
                 markdown += _markdown
         case _:
             ext = "unknown"
--- markmagic/pdf.py
+++ markmagic/pdf.py
@@ -7,10 +7,12 @@ from pypdf import PdfReader
 from markmagic.settings import Settings
 
 
-def convert_pdf(file: str | IO[bytes], settings: Settings | None = None):
+def convert_pdf(file: str | IO[bytes], settings: Settings | None = None) -> str:
     """Convert pdf to markdown."""
+    if settings is None:
+        settings = Settings()
     reader = PdfReader(file)
     markdown = ""
     for page in reader.pages:
-        markdown += page.extract_text().strip() + "\n\n"
+        markdown += page.extract_text(extraction_mode=settings.pdf.extraction_mode).strip() + "\n\n"
     return markdown.strip()
