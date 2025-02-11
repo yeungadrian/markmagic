0ad27ad35cfd7e50dc93db04d791b94bd15f5996
yeungadrian <yeungadrianjy@gmail.com>
2025-02-11 22:49:24 +0000

    fix: type stubs? for pypdfium2

---
+++ typings/pypdfium2/__init__.pyi
@@ -0,0 +1,24 @@
+"""Stub file for pyright."""
+
+from pathlib import Path
+from typing import Any
+
+from pypdfium2._helpers.bitmap import PdfBitmap
+
+class PdfPage:
+    def render(
+        self,
+        scale: float = 1,
+        rotation: int = 0,
+        crop: tuple[float, float, float, float] = (0, 0, 0, 0),
+        may_draw_forms: bool = True,
+        bitmap_maker: Any = PdfBitmap.new_native,
+        color_scheme: Any | None = None,
+        fill_to_stroke: bool = False,
+        **kwargs: Any,
+    ) -> PdfBitmap: ...
+
+class PdfDocument:
+    def __init__(self, input_data: str | Path): ...
+    def __len__(self) -> int: ...
+    def get_page(self, index: int) -> PdfPage: ...
