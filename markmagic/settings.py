1148ec78262d52c5ea949a64933a2abbb8715c6f
yeungadrian <yeungadrianjy@gmail.com>
2025-02-11 23:56:57 +0000

    chore: add test file for vlm

--- markmagic/settings.py
+++ markmagic/settings.py
@@ -17,19 +17,19 @@ class Settings(BaseSettings):
     # PDF settings
     extraction_mode: Literal["plain", "layout"] = "plain"
     use_vlm: bool = False
-    model: str = "meta-llama/Llama-Vision-Free"
+    model: str = "meta-llama/Llama-Vision-Free" #TODO: Clean up defaults?
     # model: str = "Qwen/Qwen2-VL-72B-Instruct"
     api_key: SecretStr | None = None
     base_url: str = "https://api.together.xyz/v1"
     stop: list[str] | None = ["<|eot_id|>", "<|eom_id|>"]
     # stop: list[str] | None = ["<|im_end|>", "<|endoftext|>"]
     # Table settings
-    tablefmt: str = "github"  # Type checkers do not support unpacking into a Literal
+    tablefmt: str = "github"
     showindex: bool = False
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
     @field_validator("tablefmt")
     def validate_tablefmt(cls, v: str) -> str:
         """Validate tablefmt."""
-        assert v in tabulate_formats
+        assert v in tabulate_formats # Type checkers do not support unpacking into a Literal
         return v
